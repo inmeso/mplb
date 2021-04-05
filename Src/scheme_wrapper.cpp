0714983828ef2b07aaa193f1f6d83cae60ca9ebe
Jianping Meng <jpmeng@gmail.com>
2021-04-05 09:18:25 +0000

    Modify the scheme module for data structure

--- Src/scheme_wrapper.cpp
+++ Src/scheme_wrapper.cpp
@@ -11,16 +11,20 @@ void Stream3D() {
         std::vector<int> iterRng;
         iterRng.assign(block.WholeRange().begin(), block.WholeRange().end());
         const SizeType blockIndex{block.ID()};
-        ops_par_loop(KerStream3D, "KerStream3D", block.Get(), SpaceDim(),
-                     iterRng.data(),
-                     ops_arg_dat(g_f()[blockIndex], NUMXI, LOCALSTENCIL,
-                                 "double", OPS_RW),
-                     ops_arg_dat(g_fStage()[blockIndex], NUMXI,
-                                 ONEPTLATTICESTENCIL, "double", OPS_READ),
-                     ops_arg_dat(g_NodeType()[blockIndex], NUMCOMPONENTS,
-                                 LOCALSTENCIL, "int", OPS_READ),
-                     ops_arg_dat(g_GeometryProperty()[blockIndex], 1,
-                                 LOCALSTENCIL, "int", OPS_READ));
+        for (auto& compo : g_Components()) {
+            ops_par_loop(
+                KerStream3D, "KerStream3D", block.Get(), SpaceDim(),
+                iterRng.data(),
+                ops_arg_dat(g_f().at(blockIndex), NUMXI, LOCALSTENCIL, "double",
+                            OPS_RW),
+                ops_arg_dat(g_fStage().at(blockIndex), NUMXI,
+                            ONEPTLATTICESTENCIL, "double", OPS_READ),
+                ops_arg_dat(g_NodeType().at(compo.first).at(blockIndex), 1,
+                            LOCALSTENCIL, "int", OPS_READ),
+                ops_arg_dat(g_GeometryProperty().at(blockIndex), 1,
+                            LOCALSTENCIL, "int", OPS_READ),
+                ops_arg_gbl(compo.second.index, 2, "int", OPS_READ));
+        }
     }
 }
-#endif // OPS_3D
+#endif  // OPS_3D
