553bcd9a9d50db285bcee8ad5a577c6477237395
Jianping Meng <jpmeng@gmail.com>
2021-11-11 22:49:53 +0000

    Add a 3d conservation test

---
+++ Tests/ConservationTest3D/conservation3d.cpp
@@ -0,0 +1,119 @@
+/**
+ * Copyright 2019 United Kingdom Research and Innovation
+ *
+ * Authors: See AUTHORS
+ *
+ * Contact: [jianping.meng@stfc.ac.uk and/or jpmeng@gmail.com]
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * ANDANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @brief An example main source code of stimulating 3D lid-driven cavity flow
+ *  @author Jianping Meng
+ **/
+#include <cmath>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include "mplb.h"
+#include "ops_seq_v2.h"
+#include <random>
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_real_distribution<> range(0.001,0.01);
+#include "conservation3d_kernel.inc"
+// Provide macroscopic initial conditions
+void SetInitialMacrosVars() {
+    for (auto idBlock : g_Block()) {
+        Block& block{idBlock.second};
+        std::vector<int> iterRng;
+        iterRng.assign(block.WholeRange().begin(), block.WholeRange().end());
+        const int blockIdx{block.ID()};
+        for (auto& idCompo : g_Components()) {
+            const Component& compo{idCompo.second};
+            const int rhoId{compo.macroVars.at(Variable_Rho).id};
+            ops_par_loop(KerSetInitialMacroVars, "KerSetInitialMacroVars",
+                         block.Get(), SpaceDim(), iterRng.data(),
+                         ops_arg_dat(g_MacroVars().at(rhoId).at(blockIdx), 1,
+                                     LOCALSTENCIL, "Real", OPS_RW),
+                         ops_arg_dat(g_MacroVars().at(compo.uId).at(blockIdx),
+                                     1, LOCALSTENCIL, "Real", OPS_RW),
+                         ops_arg_dat(g_MacroVars().at(compo.vId).at(blockIdx),
+                                     1, LOCALSTENCIL, "Real", OPS_RW),
+                         ops_arg_dat(g_MacroVars().at(compo.wId).at(blockIdx),
+                                     1, LOCALSTENCIL, "Real", OPS_RW),
+                         ops_arg_dat(g_CoordinateXYZ()[blockIdx], SpaceDim(),
+                                     LOCALSTENCIL, "Real", OPS_READ),
+                         ops_arg_idx());
+        }
+    }
+}
+
+int main(int argc, const char** argv) {
+    // OPS initialisation where a few arguments can be passed to set
+    // the simulation
+    ops_init(argc, argv, 4);
+    bool configFileFound{false};
+    std::string configFileName;
+    GetConfigFileFromCmd(configFileFound, configFileName, argc, argv);
+    double ct0, ct1, et0, et1;
+    ops_timers(&ct0, &et0);
+
+    ReadConfiguration(configFileName);
+    const Configuration& config{Config()};
+    DefineCase(config.caseName, config.spaceDim, config.transient);
+    DefineBlocks(config.blockIds, config.blockNames, config.blockSize,
+                 config.meshSize, config.startPos);
+    DefineComponents(config.compoNames, config.compoIds, config.lattNames,
+                     config.tauRef, config.currentTimeStep);
+    DefineMacroVars(config.macroVarTypes, config.macroVarNames,
+                    config.macroVarIds, config.macroCompoIds,
+                    config.currentTimeStep);
+    DefineInitialCondition(config.initialTypes, config.initialConditionCompoId);
+    for (auto& bcConfig : config.blockBoundaryConfig) {
+        DefineBlockBoundary(bcConfig.blockIndex, bcConfig.componentID,
+                            bcConfig.boundarySurface, bcConfig.boundaryScheme,
+                            bcConfig.macroVarTypesatBoundary,
+                            bcConfig.givenVars, bcConfig.boundaryType);
+    }
+    Partition();
+    ops_diagnostic_output();
+    SetInitialMacrosVars();
+    PreDefinedInitialCondition3D();
+    UpdateMacroVars3D();
+    CalcResidualError();
+    for (auto& compo : g_Components()) {
+        for (auto& macroVar : compo.second.macroVars) {
+            Real residualError = g_ResidualError().at(macroVar.second.id);
+            ops_printf("Residual of %s = %.17g\n", macroVar.second.name.c_str(),
+                       residualError);
+        }
+    }
+    ops_timers(&ct1, &et1);
+    ops_printf("\nTotal Wall time %lf\n", et1 - et0);
+    // Print OPS performance details to output stream
+    ops_timing_output(std::cout);
+    ops_exit();
+}
